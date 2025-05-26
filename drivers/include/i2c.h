01c602049d4c4cdbc537d9879ab93e2fd95ad443
ljyan <459436468@qq.com>
2025-05-26 03:01:33 +0000

    feat: i2c master add read and write api

--- drivers/include/i2c.h
+++ drivers/include/i2c.h
@@ -28,7 +28,7 @@ typedef struct i2c_bus
 
 void i2c_bus_init(i2c_bus_t *bus, unsigned int i2c_id, unsigned int freq);
 
-static inline int i2c_device_transfer(struct i2c_device *dev, unsigned int flag, void *data, size_t size)
+static inline int i2c_transfer(struct i2c_device *dev, unsigned int flag, void *data, size_t size)
 {
     return dev->bus->transfer(dev, flag, data, size);
 }
@@ -37,6 +37,36 @@ static inline void i2c_device_init(i2c_device_t *dev, i2c_bus_t *bus, unsigned i
 {
     dev->bus = bus;
     dev->addr = dev_addr;
+}
+
+static inline int i2c_master_write_reg8(i2c_device_t *dev, uint8_t reg, void *data, size_t size)
+{
+    int ret1 = dev->bus->transfer(dev, I2C_FLAG_WR | I2C_FLAG_NO_STOP, &reg, 1);
+    int ret2 = dev->bus->transfer(dev, I2C_FLAG_NO_START, data, size);
+    return ret1 | ret2;
+}
+
+static inline int i2c_master_read_reg8(i2c_device_t *dev, uint8_t reg, void *data, size_t size)
+{
+    int ret1 = dev->bus->transfer(dev, I2C_FLAG_WR | I2C_FLAG_NO_STOP, &reg, 1);
+    int ret2 = dev->bus->transfer(dev, I2C_FLAG_RD, data, size);
+    return ret1 | ret2;
+}
+
+static inline int i2c_master_write_reg16(i2c_device_t *dev, uint16_t reg, void *data, size_t size)
+{
+    uint8_t reg_addr[2] = {reg >> 8, reg};
+    int ret1 = dev->bus->transfer(dev, I2C_FLAG_WR | I2C_FLAG_NO_STOP, reg_addr, 2);
+    int ret2 = dev->bus->transfer(dev, I2C_FLAG_NO_START, data, size);
+    return ret1 | ret2;
+}
+
+static inline int i2c_master_read_reg16(i2c_device_t *dev, uint8_t reg, void *data, size_t size)
+{
+    uint8_t reg_addr[2] = {reg >> 8, reg};
+    int ret1 = dev->bus->transfer(dev, I2C_FLAG_WR | I2C_FLAG_NO_STOP, reg_addr, 2);
+    int ret2 = dev->bus->transfer(dev, I2C_FLAG_RD, data, size);
+    return ret1 | ret2;
 }
 
 #endif
